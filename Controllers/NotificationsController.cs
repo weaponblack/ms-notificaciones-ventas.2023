fc829c297b6e7fdb5c3e3598d9c8db99f7683538
weaponblack <juanjose5427@gmail.com>
2023-04-25 22:57:43 -0500

    conecccion con frontend

--- Controllers/NotificationsController.cs
+++ Controllers/NotificationsController.cs
@@ -61,17 +61,47 @@ public class NotificationsController : ControllerBase
         }
     }
 
+    [Route("enviar-correo-2fa")]
+    [HttpPost]
+    public async Task<ActionResult> enviarCorreo2FA(ModeloCorreo datos)
+    {
+        var apiKey = Environment.GetEnvironmentVariable("SENDGRID_API_KEY");
+        var client = new SendGridClient(apiKey);
+        
+        SendGridMessage msg = this.crearMensajeBase(datos);
+        msg.SetTemplateId(Environment.GetEnvironmentVariable("TWOFA_SENDGRID_TEMPLATE"));
+        msg.SetTemplateData(new
+        {
+            nombre = datos.nombreDestino,
+            mensaje = datos.contenidoCorreo,
+            asunto = datos.asuntoCorreo
+        });
+        var response = await client.SendEmailAsync(msg);
+
+        if (response.StatusCode == System.Net.HttpStatusCode.Accepted)
+        {
+            return Ok("Correo enviado a " + datos.correoDestino);
+        }
+        else
+        {
+            return BadRequest("Error al enviar el correo a " + datos.correoDestino);
+        }
+    }
+
     private SendGridMessage crearMensajeBase(ModeloCorreo datos)
     {
 
         var from = new EmailAddress(Environment.GetEnvironmentVariable("EMAIL_FROM"), Environment.GetEnvironmentVariable("NAME_FROM"));
         var subject = datos.asuntoCorreo;
         var to = new EmailAddress(datos.correoDestino, datos.nombreDestino);
-        var plainTextContent = "plain text content";
+        var plainTextContent = datos.contenidoCorreo;
         var htmlContent = datos.contenidoCorreo;
         var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
         return msg;
     }
+
+
+
 
 
 }
